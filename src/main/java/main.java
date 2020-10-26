34a12f3ee22a7c2b0f304141ac8fd023148ed0b8
Francisco Rafael Checa Molina <francisco.checa@autentia.com>
2020-10-26 14:49:36 +0100

    first commit

---
+++ src/main/java/main.java
@@ -0,0 +1,8 @@
+import com.furazin.projecttestgithub.Arithmetic;
+
+public class main {
+    public static void main(String[] args) {
+        Arithmetic arith = new Arithmetic(2,3);
+        System.out.println(arith.add());
+    }
+}
