7c16f56f22839344f0aff1c01790f9675082e495
Francisco Rafael Checa Molina <francisco.checa@autentia.com>
2020-10-27 13:56:35 +0100

    Added workflow file to the repository

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,7 +2,7 @@ import com.furazin.projecttestgithub.Arithmetic;
 
 public class main {
     public static void main(String[] args) {
-        Arithmetic arith = new Arithmetic(2,3);
+        Arithmetic arith = new Arithmetic(3,3);
         System.out.println(arith.add());
     }
 }
